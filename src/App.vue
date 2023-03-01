37f210d7592c218e1532b29a85b2f1145071478c
temayakovlev <artemka.yak2001@yandex.ru>
2023-03-01 17:34:52 +0300

    - Изменен дизайн - Небольшие изменения в коде

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup>
-import formLead from './components/forms/formLead.vue'
+import createLead from './components/forms/createLead.vue';
 </script>
 
 <template>
-    <div class="container-fluid">
+    <div class="container-scroller">
         <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="row">
-                            <div class="col-12 grid-margin">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="col-md-6">
-                                            <h4 class="text-left">Форма создания сделки</h4>
-                                            <formLead />
+            <div class="container-fluid page-body-wrapper">
+                <div class="main-panel">
+                    <div class="content-wrapper">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12 grid-margin stretch-card">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h4 class="card-title">Создание сделки в AmoCRM</h4>
+                                            <div class="col-md-6">
+                                                <createLead />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -22,6 +24,12 @@ import formLead from './components/forms/formLead.vue'
                         </div>
                     </div>
                 </div>
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Создано на фреймворке Vue.js 3</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">2023</span>
+                    </div>
+                </footer>
             </div>
         </main>
     </div>
--- src/components/forms/createLead.vue
+++ src/components/forms/createLead.vue
@@ -1,64 +1,65 @@
 <template>
     <form @submit.prevent="onSumbit">
         <div v-if="outputAlert.success" class="alert alert-success" role="alert">{{ outputAlert.message }}</div>
-        <div v-if="outputAlert.failed" class="alert alert-danger" role="alert">{{ outputAlert.message }}</div>
+        <div v-if="outputAlert.fail" class="alert alert-danger" role="alert">{{ outputAlert.message }}</div>
         <div class="form-group">
             <label for="form-firstName">Имя</label>
             <input class="form-control" id="form-firstName" v-model="form.firstName" v-on:blur="v$.form.firstName.$touch()">
-            <span v-if="v$.form.firstName.$error">
+            <label class="error mt-2 text-danger" v-if="v$.form.firstName.$error">
                 <template v-if="v$.form.firstName.required.$invalid">
                     Пожалуйста, укажите имя
+                </template>
+                <template v-if="v$.form.firstName.minLength.$invalid">
+                    Пожалуйста, укажите полное имя
                 </template>
                 <template v-if="v$.form.firstName.maxLength.$invalid">
                     Длина имени не должна превышать {{ v$.form.firstName.maxLength.$params.max }} символов
                 </template>
-            </span>
+            </label>
         </div>
         <div class="form-group">
             <label for="form-email">Почта</label>
             <input class="form-control" id="form-email" v-model="form.email" v-on:blur="v$.form.email.$touch()">
-            <span v-if="v$.form.email.$error">
+            <label class="error mt-2 text-danger" v-if="v$.form.email.$error">
                 <template v-if="v$.form.email.required.$invalid">
                     Пожалуйста, укажите почту
                 </template>
                 <template v-else>
                     Неправильный формат почты
                 </template>
-            </span>
+            </label>
         </div>
         <div class="form-group">
             <label for="form-telephone">Телефон</label>
             <input class="form-control" id="form-telephone" v-model="form.telephone" v-on:blur="v$.form.telephone.$touch()">
-            <span v-if="v$.form.telephone.$error">
+            <label class="error mt-2 text-danger" v-if="v$.form.telephone.$error">
                 <template v-if="v$.form.telephone.required.$invalid">
                     Пожалуйста, укажите телефон
                 </template>
                 <template v-else>
                     Неправильный формат телефона
                 </template>
-            </span>
+            </label>
         </div>
         <div class="form-group">
             <label for="form-price">Цена</label>
             <input class="form-control" id="form-price" v-model="form.price" v-on:blur="v$.form.price.$touch()">
-            <span v-if="v$.form.price.$error">
+            <label class="error mt-2 text-danger" v-if="v$.form.price.$error">
                 <template v-if="v$.form.price.required.$invalid">
                     Пожалуйста, укажите цену
                 </template>
                 <template v-else>
                     Некорректная цена
                 </template>
-            </span>
+            </label>
         </div>
-        <div class="d-flex flex-row">
-            <button type="sumbit" class="btn btn-primary mr-2 mb-2">Отправить</button>
-            <button type="cancel" class="btn btn-secondary mr-2 mb-2" disabled>Отмена</button>
-        </div>
+        <button type="sumbit" class="btn btn-primary mr-2 mb-2">Отправить</button>
     </form>
 </template>
+
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import { required, minLength, maxLength } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core';
+import { required, minLength, maxLength } from '@vuelidate/validators';
 
 export default {
     setup: () => ({ v$: useVuelidate() }),
@@ -101,18 +102,33 @@ export default {
         }
     },
     methods: {
-        formMessage(type, message) {
-            switch (type) {
-                case 'success':
-                    this.outputAlert.message = message;
-                    this.outputAlert.success = true;
-                    this.outputAlert.failed = (this.outputAlert.failed ? false : null);
-                    break;
-                case 'fail':
-                    this.outputAlert.message = message;
-                    this.outputAlert.success = (this.outputAlert.success ? false : null);
-                    this.outputAlert.failed = true;
-                    break;
+        async onSumbit() {
+            this.v$.$validate(); //валидация данных, полученные POST запросом
+            if (!(!this.v$.$error)) {
+                return;
+            };
+
+            //создание обьекта
+            let requestData = {
+                url: '/api/leads/create/',
+                data: this.getFormData(this.form)
+            };
+
+            //отправка запроса и получение ответа
+            let response = await this.sendRequest(requestData.url, requestData.data);
+            
+            //вывод блока alert
+            if (!(response)) {
+                this.outputFormAlert('fail', 'Внутренняя ошибка: обновите страницу и попробуйте еще раз');
+                return;
+            };
+            switch (response.status) {
+                case 'ok':
+                    this.outputFormAlert('success', 'Сделка оформлена, номер: ' + response.message);
+                    return;
+                default:
+                    this.outputFormAlert('fail', 'Внутренняя ошибка: попробуйте позднее');
+                    return;
             };
         },
         getFormData(object) {
@@ -121,37 +137,31 @@ export default {
                 return formData;
             }, new FormData());
         },
-        async onSumbit() {
-            this.v$.$validate();
-            if (!(!this.v$.$error)) {
-                return;
-            };
-
-            let url = 'https://browserbotdetection.ru/api/leads/create/';
-            let formData = this.getFormData(this.form);
-
-            let response = await this.sendRequest(url, formData);
-            if (!(response)) {
-                this.formMessage('fail', 'Внутренняя ошибка: обновите страницу и попробуйте еще раз');
-            };
-            switch (response.status) {
-                case 'ok':
-                    this.formMessage('success', 'Сделка оформлена, номер: ' + response.message);
+        outputFormAlert(type, message) {
+            this.outputAlert.message = message;
+            switch (type) {
+                case 'success':
+                    this.outputAlert.success = true;
+                    this.outputAlert.fail = (this.outputAlert.fail ? false : null);
+                    break;
+                case 'fail':
+                    this.outputAlert.success = (this.outputAlert.success ? false : null);
+                    this.outputAlert.fail = true;
                     break;
                 default:
-                    this.formMessage('fail', 'Внутренняя ошибка: попробуйте позднее');
-                    break;
+                    this.outputAlert.success = null;
+                    this.outputAlert.fail = null;
             };
         },
         sendRequest: async (url, formData) => {
             try {
-                let response = await fetch(url, { 
-                    method: 'POST', 
-                    body: formData 
+                let response = await fetch(url, {
+                    method: 'POST',
+                    body: formData
                 });
-                return (response.ok ? await response.json() : false);
+                return (response.ok ? await response.json() : null);
             } catch (err) {
-                return false;
+                return null;
             };
         }
     }
